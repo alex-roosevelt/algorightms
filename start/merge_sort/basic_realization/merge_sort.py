586cc6e31d3c84e1b3fb935bc5b7703a5afb056e
narynbaev.iskander <inarynbaev@beeline.kz>
2025-01-19 19:16:37 +0600

    push merge sort algorithm

---
+++ start/merge_sort/basic_realization/merge_sort.py
@@ -0,0 +1,47 @@
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+        
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+    
+    L = [0] * n1
+    R = [0] * n2
+    
+    for i in range(n1):
+        L[i] = arr[left + i]
+    
+    for j in range(n2):
+        R[j] = arr[mid + j + 1]
+    
+    i = 0
+    j = 0
+    k = left
+    
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i+=1
+        else:
+            arr[k] = R[j]
+            j+=1
+        k+=1
+    
+    while i < n1:
+        arr[k] = L[i]
+        i+=1
+        k+=1
+    
+    while j < n2:
+        arr[k] = R[j]
+        j+=1
+        k+=1
+        
+
+arr = [9,5,6,3,7,2,8,4,0,1]
+merge_sort(arr, 0, len(arr) - 1)
+print(arr) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
